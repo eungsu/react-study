ce4f53f064ff9ae75d1200ea0cdc5bea4f9a1574
eungsu <eungsu@me.com>
2025-01-06 15:16:03 +0900

    이벤트 처리

---
+++ projects/event-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import EventPractice from './EventPractice'
+
+function App() {
+
+  return (
+    <>
+      <h1>리액트 이벤트 연습</h1>
+      <EventPractice />
+    </>
+  )
+}
+
+export default App
